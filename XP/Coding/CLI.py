4ec2671e3bb09d4884b4a2449d5a2139dff8e0ef
vimofama <vimofama@outlook.com>
2023-06-28 21:51:31 -0500

    Loop del codigo

--- XP/Coding/CLI.py
+++ XP/Coding/CLI.py
@@ -1,6 +1,16 @@
 from Traductor import Traductor
 
-numero = input('Ingresa un número entre 0 y 9,999: ')
-texto_kichwa = Traductor.numero_a_kichwa(numero)
-if (texto_kichwa != None): # Evitar la impresión en caso de que no exista un retorno.
-    print('El número en kichwa es:', texto_kichwa)
+print('Bienvenido al traductor de números a kichwa.')
+flag = "s"
+while flag == "s":
+    print('--------------------------------------------------')
+    numero = input('Ingresa un número entre 0 y 9,999: ')
+    texto_kichwa = Traductor.numero_a_kichwa(numero)
+    if (texto_kichwa != None): # Evitar la impresión en caso de que no exista un retorno.
+        print('El número en kichwa es:', texto_kichwa)
+    
+    flag = input('¿Desea continuar? (s/n): ')
+
+else:
+    print('--------------------------------------------------')
+    print('Gracias por usar el traductor de números a kichwa.')
